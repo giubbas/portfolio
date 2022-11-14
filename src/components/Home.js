ed903292d7531b4997a454edce9242c8f6b5d8d4
riccardogiubilo <riccardo.giubilo@defenda.io>
2022-11-14 21:48:49 +0100

    header's typing animation added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import homeImage from '../styles/assets/home-main.jpg'
 import scroll from '../styles/assets/scroll-down.svg'
+import Typical from 'react-typical'
 
 const Home = () => {
 
   return (
     <section className='homepage'>
-      <div>
-        <h1>Hello, <br /> I'm a <span>developer</span></h1>
+      <div className='homepage-container'>
+        <div>
+          <h1>Hello, <br /> I'm <Typical className="typing-animation" loop={Infinity} steps={['Riccardo Giubilo', 1000, 'a <developer />', 1000]} /></h1>
+        </div>
         <img alt="typing" src={homeImage} />
       </div>
       <h5>scroll</h5>
